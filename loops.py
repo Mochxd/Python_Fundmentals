a66bcf34d50598db70226f990d9ced2992389885
AbdElMaksoudM1 <mohamed.abdelmaksoud@vodafone.com.egS>
2025-01-30 00:05:46 +0200

    add while loops

--- loops.py
+++ loops.py
@@ -22,15 +22,30 @@ for o in range(0, 6):
 
 print("#################################")
 """
-in case if u have only upper, lower bounds and step 
-it will start with lower and end with index -1  
+in case if u have only upper, lower bounds and step
+it will start with lower and end with index -1
 """
 for k in range(10,16,2):
     print(k)
 print("#################################")
 """
-in case if u have only upper bound it will start with 0 
+in case if u have only upper bound it will start with 0
 and end with index -1
 """
 for z in range(5):
     print(z)
+
+soso=5
+while soso<10:
+    print(soso)
+    soso+=1
+
+toto=10
+while toto>1:
+    if toto == 9:
+        toto = toto-1
+        continue
+    if toto == 3:
+        break
+    print(toto)
+    toto = toto - 1
