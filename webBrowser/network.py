e88a0f9486c5da29b04507a4e2ece2775fa319fd
Mohamed Mostafa <mm2900@fayoum.edu.eg>
2025-03-29 14:03:14 +0200

    BeautifulSoup and requests Python

---
+++ webBrowser/network.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.google.com/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+links = soup.find_all('a')
+for link in links:
+    print(link.get('href'))
