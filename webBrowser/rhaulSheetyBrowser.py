e9e1b077802c0808ee387588033ba5e6fd639533
Mohamed Mostafa <mm2900@fayoum.edu.eg>
2025-04-02 13:07:35 +0200

    handle Static drop Down menu

--- webBrowser/rhaulSheetyBrowser.py
+++ webBrowser/rhaulSheetyBrowser.py
@@ -2,13 +2,22 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
-email = (By.NAME,"email")
-password = (By.ID,"exampleInputPassword1")
+nameField = (By.NAME, "name")
+emailField = (By.NAME,"email")
+passwordField = (By.ID,"exampleInputPassword1")
 iceCreamCheckBox = (By.ID,"exampleCheck1")
+genderDropDownMenu = (By.ID, "exampleFormControlSelect1")
+studentRadioButton = (By.ID, "inlineRadio1")
+
+
+
 driver = webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/angularpractice/")
-driver.find_element(*email).send_keys("momo@gmail.com")
-driver.find_element(*password).send_keys("momo@gmail.com")
+driver.find_element(*emailField).send_keys("momo@gmail.com")
+driver.find_element(*passwordField).send_keys("momo@gmail.com")
 driver.find_element(*iceCreamCheckBox).click()
+Select(driver.find_element(*genderDropDownMenu)).select_by_visible_text("Female")
+driver.find_element(*studentRadioButton).click()
 time.sleep(2)
